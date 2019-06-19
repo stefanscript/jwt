b2c99d73949fda02cbe65ba776828fb80d15d86d
stefan <stefan.anton@mkodo.com>
2019-06-19 22:18:03 +0100

    tweaks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React from 'react';
 import './App.css';
-import {digestMessage, sha256, hexString} from "./utils";
+import {digestMessage, hexString} from "./utils";
 var jwt = require('jsonwebtoken');
 
-let token;
-sha256("1234").then(secret => {
-    token = jwt.sign({ foo: 'bar' }, secret);
-    console.info("secret", secret);
-    console.info("token", token);
-});
-
-let token2;
-digestMessage("1234").then(digest => {
-    token2 = jwt.sign({ foo: 'bar' }, hexString(digest));
-    console.info("secret2", hexString(digest));
-    console.info("token2", token2);
-});
+// let token;
+// sha256("1234").then(secret => {
+//     token = jwt.sign({ foo: 'bar' }, secret);
+//     console.info("secret", secret);
+//     console.info("token", token);
+// });
+//
+// let token2;
+// digestMessage("1234").then(digest => {
+//     token2 = jwt.sign({ foo: 'bar' }, hexString(digest));
+//     console.info("secret2", hexString(digest));
+//     console.info("token2", token2);
+// });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <pre>
-          {token}
-          </pre>
-          <pre>
-          {token2}
-          </pre>
-      </header>
-    </div>
-  );
+    let token;
+    
+    function onClickHandler(e) {
+        e.preventDefault();
+        const s = document.getElementById("secret").value;
+        digestMessage(s).then(digest => {
+            token = jwt.sign({foo: 'bar'}, hexString(digest));
+            console.log("secret", hexString(digest));
+            console.log("token", token);
+        });
+    }
+    
+    return (
+        <div className="App">
+            <header className="App-header">
+                <input name="secret" id={"secret"}/>
+                <button onClick={(e) => onClickHandler(e)}>Click</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
